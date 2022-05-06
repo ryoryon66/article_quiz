af85dfbfaac0cf410b7502e23a5b238f4184cfa0
taxi1729 <na1729taxi@gmail.com>
2022-05-06 18:59:31 +0900

    herokuへ

---
+++ test_flask.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify,request
+import utility
+from flask_cors import CORS
+ 
+app = Flask(__name__)
+CORS(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+ 
+@app.route('/')
+def hello_world():
+    return jsonify({'message': 'Hello world'})
+
+@app.route('/make_quiz',methods = ["POST"])
+def make_quiz():
+    print("hi")
+
+    material = request.json['material']  
+    print(material)
+
+
+    return jsonify({'quiz':utility.make_quiz(material)})
+    
+ 
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8888, debug=True)
--- utility.py
+++ utility.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import answer_challenge
 import spacy
 import difflib
 nlp = spacy.load("en_core_web_lg")
@@ -5,15 +6,17 @@ nlp = spacy.load("en_core_web_lg")
 
 
 def make_quiz(original_doc:str) -> str:
-    original_doc = original_doc
+    answer_key : dict[int,str] = dict()
     doc = nlp(original_doc)
     print(doc.noun_chunks)
+
+    problem_num = 1
 
     #今どこまでの文字を見ているか。
     next_index = 0
     for chunk in doc.noun_chunks:
         chunk_before = str(chunk)
-        chunk_after = ' (Q) '+str(chunk)
+        chunk_after = f' (Q{problem_num}) '+str(chunk)
 
         print(chunk,chunk[0].pos_)
 
@@ -23,7 +26,9 @@ def make_quiz(original_doc:str) -> str:
 
         
         if str(chunk[0]) in ['The','the','A','a','An','an']:
-            chunk_after = ' (Q) '+str(chunk[1:])
+            chunk_after = f' (Q{problem_num}) '+str(chunk[1:])
+        elif str(chunk[0]) in ["All","all","Both","both"]:
+            chunk_after = str(chunk[0]) + f' (Q{problem_num}) '+str(chunk[1:])
         elif chunk[0].pos_ in ['DET']:
             continue
 
@@ -36,6 +41,7 @@ def make_quiz(original_doc:str) -> str:
         next_index = next_index + left_index + len(str(chunk_after))
 
         print(original_doc)
+        problem_num += 1
 
 
 
@@ -43,48 +49,18 @@ def make_quiz(original_doc:str) -> str:
     return original_doc
 
 
-def print_diff_hl(ground_truth, target):
-    """
-    文字列の差異をハイライト表示する
-    """
-    color_dic = {
-        'red':'\033[31m',
-        'yellow':'\033[43m',  
-        'end':'\033[0m'
-    }
-
-    d = difflib.Differ()
-    diffs = d.compare(ground_truth, target)
-
-    result = ''
-    for diff in diffs:
-        status, _, character = list(diff)
-        if status == '-':
-            character = color_dic['red'] + character + color_dic['end']
-        elif status == '+':
-            character = color_dic['yellow'] + character + color_dic['end']
-        else:
-            pass
-        result += character
-
-    print(f"ground truth : {ground_truth}")
-    print(f"target string: {target}")
-    print(f"diff result  : {result}")
-
-
-
-print_diff_hl('aaa','aba')
-
 
 if __name__ == "__main__":
     document = """We created our Travel Restrictions Today visualization to look at the latest global news and determine travel bans, lockdowns, and border closures. We found datasets already cleaned and classified but they were either limited or not free of charge. So, we decided to start with the Humanitarian Data Exchange's COVID-19 Global Travel Restrictions and Airline Information dataset and create our own dataset.
 """
 
-s = input()
-if s:
-    document = s
-print(make_quiz(document))
+    s = input()
+    if s:
+        document = s
+    print(make_quiz(document))
+
+    
 
 
 
-print_diff_hl(document,make_quiz(document))
+

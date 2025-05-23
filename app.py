6f0211349c73e5ff3b488a75e86eaa269ea629c6
ryoryon66 <46624038+ryoryon66@users.noreply.github.com>
2025-05-23 22:55:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, render_template,request
 import utility
 from flask_cors import CORS
+import argparse
 
+
+
+app.run(host=args.host, port=args.port, debug=args.debug)
 app = Flask(__name__)
 CORS(app)
 
@@ -43,5 +47,10 @@ def make_tense_quiz():
     return jsonify({'quiz':utility.make_tense_quiz(material)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8889, debug = False)                                                                  
-    #app.run(host='127.0.0.1', port=8888, debug=True)   _
+    parser = argparse.ArgumentParser(description='Flask app arguments')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host address')
+    parser.add_argument('--port', type=int, default=8889, help='Port number')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug = args.debug)                                                                  
+_
